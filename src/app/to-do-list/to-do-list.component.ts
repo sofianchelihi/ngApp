cc51725634b6529111852e525a31ca5362b85e94
sofian <js_chelihi@esi.dz>
2022-10-05 13:10:19 +0100

    test

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -12,34 +12,34 @@ export class ToDoListComponent implements OnInit {
   constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<{id_tach:number,tach:string,checked:boolean}[]>("http://localhost/Serveur/AngularApp/myAngularProject/API_Server/liste.php").subscribe((res)=>{
+    this.http.get<{id_tach:number,tach:string,checked:boolean}[]>("https://unawed-gangs.000webhostapp.com/liste.php").subscribe((res)=>{
       this.liste=res;
     })
     setInterval(()=>{
-      this.http.get<any>("http://localhost/Serveur/AngularApp/myAngularProject/API_Server/liste.php").subscribe((res)=>{
+      this.http.get<any>("https://unawed-gangs.000webhostapp.com/liste.php").subscribe((res)=>{
         this.liste=res;
       })
     }, 3000);
   }
 
   check(event:{id_tach:number,tach:string,checked:boolean}){ 
-    this.http.post("http://localhost/Serveur/AngularApp/myAngularProject/API_Server/Check.php",JSON.stringify(event)).subscribe(()=>{  
+    this.http.post("https://unawed-gangs.000webhostapp.com/Check.php",JSON.stringify(event)).subscribe(()=>{  
       let index = this.liste.indexOf(event)
       if(index!=-1) this.liste[index].checked=event.checked;
     });
   }
 
   suprim(event:{id_tach:number,tach:string,checked:boolean}){
-    this.http.post("http://localhost/Serveur/AngularApp/myAngularProject/API_Server/Delet.php",JSON.stringify(event)).subscribe(()=>{  
+    this.http.post("https://unawed-gangs.000webhostapp.com/Delet.php",JSON.stringify(event)).subscribe(()=>{  
       let index = this.liste.indexOf(event);
       this.liste.splice(index,1);
     })
   }
 
   add(event:string){  
-    this.http.post<{id_tach:number,tach:string,checked:boolean}>("http://localhost/Serveur/AngularApp/myAngularProject/API_Server/AddTach.php",JSON.stringify({'event':event})).subscribe((res)=>{
+    this.http.post<{id_tach:number,tach:string,checked:boolean}>("https://unawed-gangs.000webhostapp.com/AddTach.php",JSON.stringify({'event':event})).subscribe((res)=>{
       this.liste.push(res);
+      console.log(JSON.stringify({'event':event}),res)
     })
   }
-
 }
